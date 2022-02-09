b5436321430da61c27c24f754562da6b137379c1
Richard Cotrina <rcotrina@pytel.pe>
2022-02-09 18:40:59 -0500

    Test new endpoint

--- pasarela.js
+++ pasarela.js
@@ -10,6 +10,7 @@ var form = document.getElementById("form-cc");
 form.onsubmit = function(e){
 	e.preventDefault();
 	e.stopPropagation();
+	window.app.unsetCard();
 	var submitButton = document.getElementById('cc-form-submit-btn');
 	submitButton.setAttribute("disabled", "disabled");
 	submitButton.dataset.label = submitButton.value;
@@ -31,18 +32,53 @@ form.onsubmit = function(e){
 		function(response){
 			if(!response.code){
 				var tokenInput = document.getElementById("kushki_token_input");
+				var kushkiSubscriptionIdInput = document.getElementById("kushki_subscriptionId_input");
+
 				window.app.setCard(
 					cardDetails.number.slice(-4),
-					cardDetails.expiryMonth + '/' + cardDetails.expiryYear,
-				);
-				// window.app.subscription.payment.type = null;
-				// window.app.setCard.payment.expiryDate = ;
+					cardDetails.expiryMonth + '/' + cardDetails.expiryYear);
+
+				/*
+				window.app.subscription.payment.type = null;
+				window.app.setCard.payment.expiryDate = ;
+				*/
 				tokenInput.value = response.token;
 
-				// alert("¡Perfecto! Hemos registrado tu tarjeta con éxito");
-				document.getElementById('form-cc-container').style.display = 'none';
-				window.app.validateForm(4);
-				form.reset();
+				var PAYMENT_GATEWAY_API_ENDPOINT = "https://allie-kushki.pytel.workers.dev/";
+				$.ajax({
+					url: PAYMENT_GATEWAY_API_ENDPOINT,
+					type: 'POST',
+					data: {
+						"token": response.token,
+						"startDate": FIELDS.FIRST_DELIVERY_DATE.val(),
+						"contactDetails": {
+							"documentType": FIELDS.DOCUMENT_TYPE.val(),
+							"documentNumber": FIELDS.DOCUMENT_NUMBER.val(),
+							"email": FIELDS.EMAIL.val(),
+							"firstName": FIELDS.GIVEN_NAME.val(),
+							"lastName": FIELDS.FAMILY_NAME.val(),
+							"phoneNumber": "+" + FIELDS.PHONE + FIELDS.COUNTRY_CODE
+						}
+					},
+					success: function(data){
+						if (data.subscriptionId) {
+							// alert("¡Perfecto! Hemos registrado tu tarjeta con éxito");
+							document.getElementById('form-cc-container').style.display = 'none';
+							$('input[name="medio_pago"][value="kushki"]').prop("checked", true);
+							kushkiSubscriptionIdInput.value = data.subscriptionId;
+							window.app.validateForm(4);
+							form.reset();
+						}
+					},
+					error: function(errorData){
+						console.log(errorData.message || JSON.stringify(errorData, null, 2))
+					},
+					complete: function(){
+
+					}
+				});
+
+
 			} else {
 				alert(response.message);
 				console.error('Error: ', response.error, 'Code: ', response.code, 'Message: ', response.message);
