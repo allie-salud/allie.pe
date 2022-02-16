25b8fbbfc05a4cac2cc8681d34a142b503732528
Frank Villanueva <frank.villanueva@cuida.pe>
2022-02-16 15:38:56 -0500

    debugger

--- pasarela.js
+++ pasarela.js
@@ -65,6 +65,7 @@ form.onsubmit = function(e){
 					success: function(data){
 						if (data.subscriptionId) {
 							console.log(data);
+							debugger;
 							// alert("¡Perfecto! Hemos registrado tu tarjeta con éxito");
 							document.getElementById('form-cc-container').style.display = 'none';
 							$('input[name="medio_pago"][value="kushki"]').prop("checked", true);
