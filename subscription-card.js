1f025ef99a53e8562f27b59f943925dc3b8f0f0e
Frank Villanueva <frank.villanueva@cuida.pe>
2022-02-23 15:16:42 -0500

    validate empty card

--- subscription-card.js
+++ subscription-card.js
@@ -1,2 +1,34 @@
+import { subscriptionToken } from "./pasarela";
+import { validateForm } from "./validate-form-card";
+
 let inputNumberCard = document.getElementById("number");
-inputNumberCard.setAttribute("oninput", "this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');");
+inputNumberCard.setAttribute(
+  "oninput",
+  "this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');"
+);
+
+var form = document.getElementById("form-cc");
+
+form.onsubmit = function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  window.app.unsetCard();
+  var submitButton = document.getElementById('cc-form-submit-btn');
+  submitButton.setAttribute("disabled", "disabled");
+  submitButton.dataset.label = submitButton.value;
+  submitButton.value = submitButton.dataset.wait;
+  let isValidate = validateForm(form);
+  if(isValidate){
+    let cardDetails = {
+      name: form.name.value,
+      number: form.number.value,
+      cvc: form.cvv.value,
+      expiryMonth: form.expiry_month.value,
+      expiryYear: form.expiry_year.value,
+    };
+
+    subscriptionToken(cardDetails);
+  }
+};
+
+
---
+++ validate-form-card.js
@@ -0,0 +1,21 @@
+
+const isEmpty = str => !str.trim().length;
+let inputNumberCard = document.getElementById("number");
+let labelValidateCard = document.getElementById("number-validate");
+
+export function validateForm(form) {
+  let isValidateCard = validateCard(form);
+  if(!isValidateCard){
+    return false;
+  }
+  return true;
+}
+
+function validateCard(form){
+  if(isEmpty(form.number.value)){
+    labelValidateCard.innerHTML = "El número de tu tarjeta es obligatorio";
+    inputNumberCard.classList.add("is-invalid");
+    labelValidateCard.style.display = "block";
+  }
+}
+
