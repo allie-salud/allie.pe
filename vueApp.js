0508670fb7e19c06ce20097c750c641f530da6c2
Frank Villanueva <frank.villanueva@cuida.pe>
2022-03-10 08:37:01 -0500

    fix

--- vueApp.js
+++ vueApp.js
@@ -184,7 +184,7 @@ window.app = new Vue({
             return ((this.methodSubtotal + this.productsSubtotal == 0) && this.oneTimeAmount > 0);
         },
         firstOrderAmount: function() {
-            return (this.subscriptionTotal + this.oneTimeAmount);
+            return this.subscriptionTotal + this.oneTimeAmount;
         },
         firstDeliveryTotal: function(){
             let ticketValueForDiscounts = this.methodSubtotal + this.productsSubtotal;
