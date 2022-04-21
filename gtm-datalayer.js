acbd7fff5e9b6e221efbf321d04b9f85def8350e
Frank Villanueva <frank.villanueva@cuida.pe>
2022-04-21 14:32:31 -0500

    click de prueba para tyc

--- gtm-datalayer.js
+++ gtm-datalayer.js
@@ -1,72 +1,74 @@
-let continuePersonalData = document.getElementById("continue-personal-data");
-continuePersonalData.onclick = function() {
+let continuePersonalData = document.getElementById('continue-personal-data');
+continuePersonalData.onclick = function () {
   window.dataLayer.push({
-      "virtual":{
-          "page":"/datos-personales"
-      },
-      "event":"trackVirtual"
+    virtual: {
+      page: '/datos-personales',
+    },
+    event: 'trackVirtual',
   });
 };
 
-let continueSendingData = document.getElementById("continue-sending-data");
-continueSendingData.onclick = function() {
+let continueSendingData = document.getElementById('continue-sending-data');
+continueSendingData.onclick = function () {
   window.dataLayer.push({
-      "virtual":{
-          "page":"/datos-envio"
-      },
-      "event":"trackVirtual"
+    virtual: {
+      page: '/datos-envio',
+    },
+    event: 'trackVirtual',
   });
 };
 
-let continuePaymentMethod = document.getElementById("continue-payment-method");
-continuePaymentMethod.onclick = function() {
+let continuePaymentMethod = document.getElementById('continue-payment-method');
+continuePaymentMethod.onclick = function () {
   window.dataLayer.push({
-      "virtual":{
-          "page":"/metodo-pago"
-      },
-      "event":"trackVirtual"
+    virtual: {
+      page: '/metodo-pago',
+    },
+    event: 'trackVirtual',
   });
 };
-
 
 let paymentMethodOptions = document.querySelectorAll('[name="medio_pago"]');
-paymentMethodOptions.forEach(function(item) {
-  item.onclick = function() {
-    const paymentMethod = item.getAttribute("value");
-    let textValue = "";
+paymentMethodOptions.forEach(function (item) {
+  item.onclick = function () {
+    const paymentMethod = item.getAttribute('value');
+    let textValue = '';
     switch (paymentMethod) {
-      case "pos":
-        textValue = "POS contra entrega";
+      case 'pos':
+        textValue = 'POS contra entrega';
         break;
-      case "yape":
-        textValue = "Yape";
+      case 'yape':
+        textValue = 'Yape';
         break;
-      case "transferencia":
-        textValue = "Transferencia";
+      case 'transferencia':
+        textValue = 'Transferencia';
         break;
-      case "pagolink":
-        textValue = "Pago Link / Enlace de pago";
+      case 'pagolink':
+        textValue = 'Pago Link / Enlace de pago';
         break;
     }
     window.dataLayer.push({
-        "action":{
-            "category": "Método de pago",
-            "name": "Elegir medio de pago",
-            "label": textValue
-        },
-        "event":"trackAction"
+      action: {
+        category: 'Método de pago',
+        name: 'Elegir medio de pago',
+        label: textValue,
+      },
+      event: 'trackAction',
     });
   };
 });
 
-
-
-let recurringCardPayment = document.getElementById("recurring-card-payment");
-recurringCardPayment.onclick = function() {
+let recurringCardPayment = document.getElementById('recurring-card-payment');
+recurringCardPayment.onclick = function () {
   window.dataLayer.push({
-      "virtual":{
-          "page":"/ingresar-tarjeta"
-      },
-      "event":"trackVirtual"
+    virtual: {
+      page: '/ingresar-tarjeta',
+    },
+    event: 'trackVirtual',
   });
+};
+
+let acceptTyc = document.getElementById('accept-tyc');
+acceptTyc.onclick = function () {
+  alert('Button clicked');
 };
